f7c0cc076c7ecececa89811a8b24a84d4350251e
Neill Bogie <neillbogie@googlemail.com>
2015-08-23 02:27:41 +0100

    add a minimal sketch which plays gibber drums

---
+++ gibbertest/drumSketchMinimal.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// move your mouse in the window to change audiovisuals
+function setup() {
+  createCanvas( windowWidth, windowHeight )
+
+  EDrums( 'x*o*-*o-' )
+
+}
+
+function draw() {
+
+}
